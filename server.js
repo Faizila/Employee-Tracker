cf420f06a426c826584345bc0663ba7ad4759ff8
Faizila Amlani <amlani.faizila@gmail.com>
2021-10-21 18:17:57 -0500

    added dependencies and connect to database code in server.js, added initial files with code for creating a database in schema.sql

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      port: 3306,
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: '123456',
+      database: 'employees_db'
+    },
+    console.log("***********************************"),
+    console.log("*           WELCOME TO            *"),
+    console.log("*        EMPLOYEE TRACKER         *"),
+    console.log("*                                 *"),
+    console.log("***********************************")
+  );
+
