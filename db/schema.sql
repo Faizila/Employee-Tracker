d426534c960752039c5b12cc11699a04b547cec0
Faizila Amlani <amlani.faizila@gmail.com>
2021-10-21 18:38:08 -0500

    created three tables in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,26 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 
 USE employees_db;
+
+-- Create Department table --
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+    );
+
+-- Create Role Table --
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
+   );
+
+-- Create Employee Table --
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
+);
