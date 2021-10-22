6cfde260d8e747669cc45bd17b7b42b1cf25ce2d
Faizila Amlani <amlani.faizila@gmail.com>
2021-10-22 15:47:46 -0500

    connected tables in schema.sql by adding primary and foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,42 @@
+-- Delete database
 DROP DATABASE IF EXISTS employees_db;
+-- Create database
 CREATE DATABASE employees_db;
-
+-- use database
 USE employees_db;
 
+-- delete table with this name if exist
 DROP TABLE IF EXISTS department;
+-- Create table
 CREATE TABLE department (
-  id INT NOT NULL,
-  name VARCHAR(30) NOT NULL
+  id INT NOT NULL AUTO_INCREMENT,
+  dept_name VARCHAR(30),
+  PRIMARY KEY (id)
 );
 
+-- delete table with this name if exist
 DROP TABLE IF EXISTS role;
+-- Create table
 CREATE TABLE role (
-  id INT NOT NULL,
+  role_id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
+  salary DECIMAL (10,2) NOT NULL,
   department_id INT NOT NULL
+  PRIMARY KEY (role_id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
+-- delete table with this name if exist
 DROP TABLE IF EXISTS employee;
+-- Create table
 CREATE TABLE employee (
-  id INT NOT NULL,
+  emp_id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT
+  emp_role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (emp_id),
+  FOREIGN KEY (emp_role_id) REFERENCES role(role_id),
+  FOREIGN KEY (manager_id) REFERENCES employee(emp_id)
 );
 
